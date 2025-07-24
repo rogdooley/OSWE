483010b6cd525eeee65e382e10506cc0132da5da
Roger Dooley <rogdooley@gmail.com>
2025-07-24 09:59:03 -0400

    channging the python skeleton to something more suited for oswe

--- TemplatesAndSnippets/boilerplate.py
+++ TemplatesAndSnippets/boilerplate.py
@@ -1,19 +1,50 @@
 import argparse
+import requests
+
+from pathlib import Path
+
+root_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(root_dir))
+
+# If commomn isn't in ../../common, change root dir or change the imports as appropriate
+from common.offsec_logger import OffsecLogger
+from common.file_transfer_server import FileTransferServer
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Description of your script.")
-    
-    parser.add_argument("-i", "--input", type=str, required=True, help="Input file path")
-    parser.add_argument("-o", "--output", type=str, required=True, help="Output file path")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
-    
+    parser = argparse.ArgumentParser(description="OSWE Chat Application Exploit.")
+
+    parser.add_argument("--target", type=str, required=True, help="Input file path")
+    parser.add_argument(
+        "--password",
+        nargs="?",
+        const=None,
+        help="Password for username. If left blank, a random password will be generated.",
+    )
+    parser.add_argument(
+        "--password-length",
+        type=int,
+        default=16,
+        help="Length of the password if generated.",
+    )
+    parser.add_argument(
+        "--listening-port",
+        type=int,
+        default=9001,
+        help="Port to listen for reverse shell (default: 9001)",
+    )
+    parser.add_argument(
+        "--listening-ip", type=str, help="IP to listen on for reverse shell"
+    )
+
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
-    
-    # Your main code logic here
+
+    # ! TODO
+
 
 if __name__ == "__main__":
     main()
-
