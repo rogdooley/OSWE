7e066904479f22f90c2a5e2bf87c8d151755b90d
Roger Dooley <rogdooley@gmail.com>
2025-07-28 22:10:07 -0400

    adding in terminal launching for listening for reverse shells

--- TemplatesAndSnippets/boilerplate.py
+++ TemplatesAndSnippets/boilerplate.py
@@ -2,6 +2,8 @@ import argparse
 import requests
 import sys
 import json
+import shutil
+import subprocess
 
 from pathlib import Path
 from dataclasses import dataclass, asdict, field
@@ -13,9 +15,6 @@ sys.path.append(str(root_dir))
 # If commomn isn't in ../../common, change root dir or change the imports as appropriate
 from common.offsec_logger import OffsecLogger
 from common.file_transfer_server import FileTransferServer
-from common.data_faker import DataFaker
-
-logger = OffsecLogger(logfile="exploit.log", debug=True)
 
 
 @dataclass
@@ -59,11 +58,33 @@ class ExploitContext:
                 setattr(self, key, value)
 
 
+def spawn_external_listener(listening_port: int):
+    cmd = f"bash -c 'nc -lvnp {listening_port}; echo Press enter to close; read'"
+    if shutil.which("gnome-terminal"):
+        subprocess.Popen(["gnome-terminal", "--", "bash", "-c", cmd])
+    elif shutil.which("x-terminal-emulator"):
+        subprocess.Popen(["x-terminal-emulator", "-e", cmd])
+    else:
+        print(f"[!] No terminal available. Run manually:\n{cmd}")
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="OSWE Chat Application Exploit.")
 
     parser.add_argument("--target-ip", type=str, required=True, help="Input file path")
     parser.add_argument("--target-port", type=int, default=80, help="Input file path")
+    parser.add_argument(
+        "--password",
+        nargs="?",
+        const=None,
+        help="Password for username. If left blank, a random password will be generated.",
+    )
+    parser.add_argument(
+        "--password-length",
+        type=int,
+        default=16,
+        help="Length of the password if generated.",
+    )
     parser.add_argument(
         "--listening-port",
         type=int,
@@ -95,7 +116,7 @@ def main():
         target_ip=args.target_ip,
         target_port=args.target_port,
         attacker_ip=args.listening_ip,
-        attacker_port=args.listening_port,
+        attacker_port=args.lstening_port,
         protocol="http",
         vuln_name="<INSERT>",
         poc_id="<INSERT>",
