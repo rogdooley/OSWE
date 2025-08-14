0b550580f2fceda78aa252a708f23e1595657208
Roger Dooley <rogdooley@gmail.com>
2025-08-13 23:57:54 -0400

    removing unneeded code

--- TemplatesAndSnippets/IdentityGenerator/providers/base.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol, Mapping, Any
-import random
-
-
-class Provider(Protocol):
-    name: str
-
-    def generate(self, rng: random.Random, overrides: Mapping[str, Any]) -> Any: ...
